002957cac484e26608f52780766484f04d38b1c2
liuyuan <2787141886@qq.com>
2022-05-05 15:39:38 +0800

    GDB: added and finished

---
+++ GDB/TestCodes/2_DebugRunningProgramTest/TestCode.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+void test()
+{
+
+}
+
+void test1()
+{
+	int i = 0;
+	i++;
+}
+
+int main()
+{
+	for(;;)
+	{
+		test();
+		test1();
+	}
+
+	return 0;
+}
