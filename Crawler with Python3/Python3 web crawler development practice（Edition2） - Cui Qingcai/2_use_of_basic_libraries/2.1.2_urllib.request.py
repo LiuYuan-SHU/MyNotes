09146644716b1519151990eed4fb056de1ef6eb3
Liuyuan <2787141886@qq.com>
2022-06-26 23:36:07 +0800

    Finished: 2.1.2

--- Crawler with Python3/Python3 web crawler development practice（Edition2） - Cui Qingcai/2_use_of_basic_libraries/2.1.2_urllib.request.py
+++ Crawler with Python3/Python3 web crawler development practice（Edition2） - Cui Qingcai/2_use_of_basic_libraries/2.1.2_urllib.request.py
@@ -1,11 +1,22 @@
 import urllib.request
 
-# 如果需要往请求中加入Headers等信息，就需要Request类来构建请求
+def request_test():
+    request = urllib.request.Request('https://python.org')
+    response = urllib.request.urlopen(request)
+    print(response.read().decode('utf-8'))
 
-# 1. 通过独立构造Request对象，我们可以：
-#   1.1 将请求独立生成成一个对象
-#   1.2 更加丰富和灵活地配置参数
-request = urllib.request.Request('https://python.org')
+from urllib import request, parse
+def multi_parameter_request():
+    url = 'https://www.httpbin.org/post'
+    headers = {
+        'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
+        'Host': 'www.httpbin.org'
+    }
+    dict = { 'name': 'germey' }
+    data = bytes(parse.urlencode(dict), encoding = 'utf-8')
+    req = request.Request(url = url, data = data, headers = headers, method = 'POST')
+    response = request.urlopen(req)
+    print(response.read().decode('utf-8'))
 
-response = urllib.request.urlopen(request)
-print(response.read().decode('utf-8'))
+# request_test()
+multi_parameter_request()
