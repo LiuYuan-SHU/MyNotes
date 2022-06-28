774db9dd5faca0f1b83f509ecad0b304b47a2543
Liuyuan <2787141886@qq.com>
2022-06-28 23:27:29 +0800

    update 2.2.1

--- Crawler with Python3/Python3 web crawler development practice（Edition2） - Cui Qingcai/2_use_of_basic_libraries/2.2/2.2.1_get.py
+++ Crawler with Python3/Python3 web crawler development practice（Edition2） - Cui Qingcai/2_use_of_basic_libraries/2.2/2.2.1_get.py
@@ -1,4 +1,5 @@
 from requests import get
+import re                   # 正则表达式库
 
 def get_test():
     data = {
@@ -14,5 +15,27 @@ def get_json():
     print(r.json())
     print(type(r.json()))
 
+def get_web():
+    r = get('https://ssr1.scrape.center/')
+    pattern = re.compile('<h2.*?>(.*?)</h2>', re.S)
+    titles = re.findall(pattern, r.text)
+    print(titles)
+
+def get_binary():
+    r = get('https://scrape.center/favicon.ico')
+    with open('favicon.ico', 'wb') as f:
+        f.write(r.content)
+
+def add_headers():
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/52.0.2743.116 Safari/    537.36 '
+    }
+    response = get('https://ssr1.scrape.center/', headers=headers)
+    print(response.text)
+
 # get_test()
-get_json()
+# get_json()
+# get_web()
+# get_binary()
+add_headers()
