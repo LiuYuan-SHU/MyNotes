3e33dcd84c2db4f69d1ec2d35b20fe49eea4e95b
Liuyuan <2787141886@qq.com>
2022-07-17 15:59:55 +0800

    update 3.1.4

--- Crawler with Python3/Python3 web crawler development practice（Edition2） - Cui Qingcai/第三章/3.1/3.1.4_父节点.py
+++ Crawler with Python3/Python3 web crawler development practice（Edition2） - Cui Qingcai/第三章/3.1/3.1.4_父节点.py
@@ -3,3 +3,5 @@ from lxml import etree
 html = etree.parse('./test.html', etree.HTMLParser())
 result = html.xpath('//a[@href="link4.html"]/../@class')
 print(result)
+result = html.xpath('//a[@href="link4.html"]/parent::*/@class')
+print(result)
