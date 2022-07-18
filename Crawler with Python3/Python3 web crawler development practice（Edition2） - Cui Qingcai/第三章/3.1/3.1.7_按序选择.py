2b9c72df975bf29a598e357fb06d0bda9ade7eca
Liuyuan <2787141886@qq.com>
2022-07-18 18:40:54 +0800

    create 3.1.7

---
+++ Crawler with Python3/Python3 web crawler development practice（Edition2） - Cui Qingcai/第三章/3.1/3.1.7_按序选择.py
@@ -0,0 +1,11 @@
+from lxml import etree
+
+html = etree.parse('./test.html', etree.HTMLParser())
+result = html.xpath('//li[1]/a/text()')
+print(result)
+result = html.xpath('//li[last()]/a/text()')
+print(result)
+result = html.xpath('//li[position()<3]/a/text()')
+print(result)
+result = html.xpath('//li[last()-2]/a/text()')
+print(result)
