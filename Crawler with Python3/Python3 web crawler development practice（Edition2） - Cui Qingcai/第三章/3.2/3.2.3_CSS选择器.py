ef7c21f16dba35f8092d35f5d2201c774871295a
Liuyuan <2787141886@qq.com>
2022-07-19 14:24:44 +0800

    create 3.2.3

---
+++ Crawler with Python3/Python3 web crawler development practice（Edition2） - Cui Qingcai/第三章/3.2/3.2.3_CSS选择器.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+
+html = '''
+<div class="panel">
+    <div class="panel-heading">
+        <h4>Hello</4>
+    </div>
+    <div class="panel-body">
+        <ul class="list" id="list-1">
+            <li class="element">Foo</li>
+            <li class="element">Bar</li>
+            <li class="element">Jay</li>
+        </ul>
+        <ul class="list list-small" id="list-2">
+            <li class="element">Foo</li>
+            <li class="element">Bar</li>
+        </ul>
+    </div>
+</div>
+'''
+
+soup = BeautifulSoup(html, 'lxml')
+print(soup.select('.panel .panel-heading'))
+print(soup.select('ul li'))
+print(soup.select('#list-2 .element'))
+print(type(soup.select('ul')[0]))
