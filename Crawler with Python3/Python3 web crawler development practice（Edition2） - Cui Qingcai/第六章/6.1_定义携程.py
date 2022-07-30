79a5632bb8c2c16e4e1f0b683eeaa883082b7029
Liuyuan <2787141886@qq.com>
2022-07-30 16:31:05 +0800

    update 6.1

--- Crawler with Python3/Python3 web crawler development practice（Edition2） - Cui Qingcai/第六章/6.1_定义携程.py
+++ Crawler with Python3/Python3 web crawler development practice（Edition2） - Cui Qingcai/第六章/6.1_定义携程.py
@@ -1,12 +1,15 @@
 import asyncio
 
-async def execute(x):
-    print('Number: ',x)
 
+async def execute(x):
+    print('Number: ', x)
+
+# 定义协程对象，但此时并没有开始执行
 coroutine = execute(1)
 print('Coroutine:', coroutine)
 print('After calling execute')
 
+# 创建loop，让协程对象在其中执行
 loop = asyncio.get_event_loop()
 loop.run_until_complete(coroutine)
 print('After calling loop')
